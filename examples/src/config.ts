1254e8d4f45db03c032605cb0571eae473605251
rikka <admin@rikka.cc>
2025-08-07 15:11:21 +0800

    fix: update maptiler_key to correct value

--- examples/src/config.ts
+++ examples/src/config.ts
@@ -1,7 +1,6 @@
 const config = {
   // maptiler_key: 'get_your_own_OpIi9ZULNHzrESv6T2vL',
-  // maptiler_key: 'GhNJDabtJW2KhnZWhdq0',
-  maptiler_key: 'UWryUlitguqPCwnH40QT',
+  maptiler_key: 'GhNJDabtJW2KhnZWhdq0',
 }
 
 export default config
