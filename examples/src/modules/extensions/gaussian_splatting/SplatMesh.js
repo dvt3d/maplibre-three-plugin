dd08c6e63a299aba2c15bf7133eceb6dabe6f9c6
cavencj <cavencj@gmail.com>
2025-08-20 17:37:06 +0800

    update splat mesh

--- examples/src/modules/extensions/gaussian_splatting/SplatMesh.js
+++ examples/src/modules/extensions/gaussian_splatting/SplatMesh.js
@@ -5,7 +5,11 @@ import gaussian_splatting_vs_glsl from '../../shaders/gaussian_splatting_vs_glsl
 import gaussian_splatting_fs_glsl from '../../shaders/gaussian_splatting_fs_glsl.js'
 import { doSplatSort } from '../../workers/SplatSortWorker.js'
 
-const rowLength = 3 * 4 + 3 * 4 + 4 + 4
+const cov_indexes = [0, 1, 2, 5, 6, 10]
+
+const _center = new THREE.Vector3()
+const _scale = new THREE.Vector3()
+const _quaternion = new THREE.Quaternion()
 
 class SplatMesh extends THREE.Mesh {
   constructor(numVertexes) {
@@ -57,6 +61,8 @@ class SplatMesh extends THREE.Mesh {
 
     this._loadedVertexCount = 0
 
+    this._threshold = -0.00001
+
     const baseGeometry = new BufferGeometry()
     const positions = new BufferAttribute(
       new Float32Array([
@@ -98,11 +104,12 @@ class SplatMesh extends THREE.Mesh {
       depthWrite: false,
       transparent: true,
     })
+
     this.material.onBeforeRender = this._onMaterialBeforeRender.bind(this)
+
     this.frustumCulled = false
 
     this._positions = new Float32Array(0)
-    this.threshold = -0.00001
 
     this._isSortting = false
 
@@ -111,6 +118,14 @@ class SplatMesh extends THREE.Mesh {
 
   get isSplatMesh() {
     return true
+  }
+
+  set threshold(threshold) {
+    this._threshold = threshold
+  }
+
+  get threshold() {
+    return this._threshold
   }
 
   get bounds() {
@@ -160,164 +175,10 @@ class SplatMesh extends THREE.Mesh {
 
   /**
    *
-   * @param numPoints
-   * @param positions
-   * @param scales
-   * @param colors
-   * @param alphas
-   * @param rotations
-   * @returns {Uint8Array<ArrayBuffer>}
-   * @private
-   */
-  _getBytesFromAttribute(
-    numPoints,
-    positions,
-    scales,
-    colors,
-    alphas,
-    rotations
-  ) {
-    const buffer = new ArrayBuffer(numPoints * rowLength)
-
-    const bytes = new Uint8Array(buffer)
-
-    const dataView = new DataView(buffer)
-
-    const hasAlphaInColors = colors.length === numPoints * 4
-
-    const colorByte = (v) =>
-      Util.clamp(v <= 1 ? Math.round(v * 255) : Math.round(v), 0, 255)
-
-    const quatByte = (v) => {
-      const u = Util.clamp(v, -1, 1) * 128 + 128
-      return Util.clamp(Math.round(u), 0, 255)
-    }
-
-    for (let i = 0; i < numPoints; i++) {
-      const offset = i * rowLength
-
-      //set positions
-      dataView.setFloat32(offset + 0, positions[3 * i + 0], true)
-      dataView.setFloat32(offset + 4, positions[3 * i + 1], true)
-      dataView.setFloat32(offset + 8, positions[3 * i + 2], true)
-      //set scales
-
-      dataView.setFloat32(offset + 12, scales[3 * i + 0], true)
-      dataView.setFloat32(offset + 16, scales[3 * i + 1], true)
-      dataView.setFloat32(offset + 20, scales[3 * i + 2], true)
-
-      // set colors
-      const colorOffset = hasAlphaInColors ? 4 * i : 3 * i
-      bytes[offset + 24] = colorByte(colors[colorOffset + 0])
-      bytes[offset + 25] = colorByte(colors[colorOffset + 1])
-      bytes[offset + 26] = colorByte(colors[colorOffset + 2])
-      bytes[offset + 27] = colorByte(
-        hasAlphaInColors ? colors[4 * i + 3] : alphas[i]
-      )
-      // set rotations
-      bytes[offset + 28] = quatByte(rotations[4 * i + 3])
-      bytes[offset + 29] = quatByte(rotations[4 * i + 0])
-      bytes[offset + 30] = quatByte(rotations[4 * i + 1])
-      bytes[offset + 31] = quatByte(rotations[4 * i + 2])
-    }
-    return bytes
-  }
-
-  /**
-   *
-   * @param buffer
    * @param vertexCount
    * @private
    */
-  _pushDataToTexture(buffer, vertexCount) {
-    if (this._loadedVertexCount + vertexCount > this._maxVertexes) {
-      console.log('vertexCount limited to ', this._maxVertexes, vertexCount)
-      vertexCount = this._maxVertexes - this._loadedVertexCount
-    }
-    if (vertexCount <= 0) {
-      return
-    }
-    let u_buffer = new Uint8Array(buffer)
-    let f_buffer = new Float32Array(buffer)
-
-    let new_positions = new Float32Array(vertexCount * 4)
-
-    const rotationAndColorData_uint8 = new Uint8Array(
-      this._rotationAndColorData.buffer
-    )
-
-    const rotationAndColorData_int16 = new Int16Array(
-      this._rotationAndColorData.buffer
-    )
-
-    for (let i = 0; i < vertexCount; i++) {
-      let quaternion = new THREE.Quaternion(
-        (u_buffer[32 * i + 28 + 1] - 128) / 128.0,
-        (u_buffer[32 * i + 28 + 2] - 128) / 128.0,
-        -(u_buffer[32 * i + 28 + 3] - 128) / 128.0,
-        (u_buffer[32 * i + 28 + 0] - 128) / 128.0
-      )
-
-      let center = new THREE.Vector3(
-        f_buffer[8 * i + 0],
-        f_buffer[8 * i + 1],
-        -f_buffer[8 * i + 2]
-      )
-
-      let scale = new THREE.Vector3(
-        f_buffer[8 * i + 3 + 0],
-        f_buffer[8 * i + 3 + 1],
-        f_buffer[8 * i + 3 + 2]
-      )
-
-      let mtx = new THREE.Matrix4()
-      mtx.makeRotationFromQuaternion(quaternion)
-      mtx.transpose()
-      mtx.scale(scale)
-      let mtx_t = mtx.clone()
-      mtx.transpose()
-      mtx.premultiply(mtx_t)
-      mtx.setPosition(center)
-
-      let cov_indexes = [0, 1, 2, 5, 6, 10]
-      let max_value = 0.0
-      for (let j = 0; j < cov_indexes.length; j++) {
-        if (Math.abs(mtx.elements[cov_indexes[j]]) > max_value) {
-          max_value = Math.abs(mtx.elements[cov_indexes[j]])
-        }
-      }
-
-      let destOffset = this._loadedVertexCount * 4 + i * 4
-      this._centerAndScaleData[destOffset + 0] = center.x
-      this._centerAndScaleData[destOffset + 1] = center.y
-      this._centerAndScaleData[destOffset + 2] = center.z
-      this._centerAndScaleData[destOffset + 3] = max_value / 32767.0
-
-      destOffset = this._loadedVertexCount * 8 + i * 4 * 2
-
-      for (let j = 0; j < cov_indexes.length; j++) {
-        rotationAndColorData_int16[destOffset + j] = parseInt(
-          (mtx.elements[cov_indexes[j]] * 32767.0) / max_value + ''
-        )
-      }
-
-      // RGBA
-      destOffset = this._loadedVertexCount * 16 + (i * 4 + 3) * 4
-
-      rotationAndColorData_uint8[destOffset + 0] = u_buffer[32 * i + 24 + 0]
-      rotationAndColorData_uint8[destOffset + 1] = u_buffer[32 * i + 24 + 1]
-      rotationAndColorData_uint8[destOffset + 2] = u_buffer[32 * i + 24 + 2]
-      rotationAndColorData_uint8[destOffset + 3] = u_buffer[32 * i + 24 + 3]
-
-      // Store scale and transparent to remove splat in sorting process
-      new_positions[i * 4 + 0] = mtx.elements[12]
-      new_positions[i * 4 + 1] = mtx.elements[13]
-      new_positions[i * 4 + 2] = mtx.elements[14]
-      new_positions[i * 4 + 3] =
-        (Math.max(scale.x, scale.y, scale.z) * u_buffer[32 * i + 24 + 3]) /
-        255.0
-    }
-
+  _updateTexture(vertexCount) {
     while (vertexCount > 0) {
       const xOffset = this._loadedVertexCount % this._textureWidth
       const yOffset = Math.floor(this._loadedVertexCount / this._textureWidth)
@@ -334,23 +195,217 @@ class SplatMesh extends THREE.Mesh {
       const dstOffset = (yOffset * this._textureWidth + xOffset) * 4
       const srcOffset = this._loadedVertexCount * 4
       const copyLength = pixelsToWrite * 4
-
       this._centerAndScaleTexture.image.data.set(
         this._centerAndScaleData.subarray(srcOffset, srcOffset + copyLength),
         dstOffset
       )
-
       this._rotationAndColorTexture.image.data.set(
         this._rotationAndColorData.subarray(srcOffset, srcOffset + copyLength),
         dstOffset
       )
-
       this._loadedVertexCount += pixelsToWrite
       vertexCount -= pixelsToWrite
     }
-
     this._centerAndScaleTexture.needsUpdate = true
     this._rotationAndColorTexture.needsUpdate = true
+  }
+
+  _updateCenterAndScaleData(index, mtx, center, scale, quaternion) {
+    mtx.makeRotationFromQuaternion(quaternion)
+    mtx.transpose()
+    mtx.scale(scale)
+    let mtx_t = mtx.clone()
+    mtx.transpose()
+    mtx.premultiply(mtx_t)
+    mtx.setPosition(center)
+
+    let max_value = 0.0
+
+    for (let j = 0; j < cov_indexes.length; j++) {
+      if (Math.abs(mtx.elements[cov_indexes[j]]) > max_value) {
+        max_value = Math.abs(mtx.elements[cov_indexes[j]])
+      }
+    }
+
+    let destOffset = this._loadedVertexCount * 4 + index * 4
+    this._centerAndScaleData[destOffset + 0] = center.x
+    this._centerAndScaleData[destOffset + 1] = center.y
+    this._centerAndScaleData[destOffset + 2] = center.z
+    this._centerAndScaleData[destOffset + 3] = max_value / 32767.0
+
+    return max_value
+  }
+
+  /**
+   *
+   * @param buffer
+   * @param vertexCount
+   * @private
+   */
+  _updateDataFromBuffer(buffer, vertexCount) {
+    if (this._loadedVertexCount + vertexCount > this._maxVertexes) {
+      console.log('vertexCount limited to ', this._maxVertexes, vertexCount)
+      vertexCount = this._maxVertexes - this._loadedVertexCount
+    }
+    if (vertexCount <= 0) {
+      return
+    }
+    let u_buffer = new Uint8Array(buffer)
+    let f_buffer = new Float32Array(buffer)
+    let new_positions = new Float32Array(vertexCount * 4)
+
+    const rotationAndColorData_uint8 = new Uint8Array(
+      this._rotationAndColorData.buffer
+    )
+    const rotationAndColorData_int16 = new Int16Array(
+      this._rotationAndColorData.buffer
+    )
+
+    for (let i = 0; i < vertexCount; i++) {
+      _center.set(
+        f_buffer[8 * i + 0],
+        f_buffer[8 * i + 1],
+        -f_buffer[8 * i + 2]
+      )
+      _scale.set(
+        f_buffer[8 * i + 3 + 0],
+        f_buffer[8 * i + 3 + 1],
+        f_buffer[8 * i + 3 + 2]
+      )
+      _quaternion.set(
+        (u_buffer[32 * i + 28 + 1] - 128) / 128.0,
+        (u_buffer[32 * i + 28 + 2] - 128) / 128.0,
+        -(u_buffer[32 * i + 28 + 3] - 128) / 128.0,
+        (u_buffer[32 * i + 28 + 0] - 128) / 128.0
+      )
+      let mtx = new THREE.Matrix4()
+      let max_value = this._updateCenterAndScaleData(
+        i,
+        mtx,
+        _center,
+        _scale,
+        _quaternion
+      )
+      let destOffset = this._loadedVertexCount * 8 + i * 4 * 2
+
+      for (let j = 0; j < cov_indexes.length; j++) {
+        rotationAndColorData_int16[destOffset + j] = parseInt(
+          (mtx.elements[cov_indexes[j]] * 32767.0) / max_value
+        )
+      }
+      // RGBA
+      destOffset = this._loadedVertexCount * 16 + (i * 4 + 3) * 4
+      rotationAndColorData_uint8[destOffset + 0] = u_buffer[32 * i + 24 + 0]
+      rotationAndColorData_uint8[destOffset + 1] = u_buffer[32 * i + 24 + 1]
+      rotationAndColorData_uint8[destOffset + 2] = u_buffer[32 * i + 24 + 2]
+      rotationAndColorData_uint8[destOffset + 3] = u_buffer[32 * i + 24 + 3]
+
+      // Store scale and transparent to remove splat in sorting process
+      new_positions[i * 4 + 0] = mtx.elements[12]
+      new_positions[i * 4 + 1] = mtx.elements[13]
+      new_positions[i * 4 + 2] = mtx.elements[14]
+      new_positions[i * 4 + 3] =
+        (Math.max(_scale.x, _scale.y, _scale.z) * u_buffer[32 * i + 24 + 3]) /
+        255.0
+    }
+
+    let temp = new Float32Array(this._positions.length + new_positions.length)
+    temp.set(this._positions)
+    temp.set(new_positions, this._positions.length)
+    this._positions = temp
+  }
+
+  /**
+   *
+   * @param spzData
+   * @private
+   */
+  _updateDataFromSpz(spzData) {
+    let vertexCount = spzData.numPoints
+    const positions = spzData.positions
+    const colors = spzData.colors
+    const scales = spzData.scales
+    const alphas = spzData.alphas
+    const rotations = spzData.rotations
+    if (this._loadedVertexCount + vertexCount > this._maxVertexes) {
+      console.log('vertexCount limited to ', this._maxVertexes, vertexCount)
+      vertexCount = this._maxVertexes - this._loadedVertexCount
+    }
+    if (vertexCount <= 0) {
+      return
+    }
+  }
+
+  /**
+   *
+   * @param geometry
+   * @private
+   */
+  _updateDataFromGeometry(geometry) {
+    let vertexCount = geometry.attributes.position.count
+    if (this._loadedVertexCount + vertexCount > this._maxVertexes) {
+      console.log('vertexCount limited to ', this._maxVertexes, vertexCount)
+      vertexCount = this._maxVertexes - this._loadedVertexCount
+    }
+    if (vertexCount <= 0) {
+      return
+    }
+    const positions = geometry.attributes.position.array
+    const scales = geometry.attributes._scale.array
+    const colors = geometry.attributes.color.array
+    const rotations = geometry.attributes._rotation.array
+    let new_positions = new Float32Array(vertexCount * 4)
+    const rotationAndColorData_uint8 = new Uint8Array(
+      this._rotationAndColorData.buffer
+    )
+    const rotationAndColorData_int16 = new Int16Array(
+      this._rotationAndColorData.buffer
+    )
+
+    for (let i = 0; i < vertexCount; i++) {
+      _center.set(
+        positions[3 * i + 0],
+        positions[3 * i + 1],
+        -positions[3 * i + 2]
+      )
+      _scale.set(scales[3 * i + 0], scales[3 * i + 1], scales[3 * i + 2])
+      _quaternion.set(
+        rotations[i * 4 + 1],
+        rotations[i * 4 + 2],
+        -rotations[i * 4 + 3],
+        rotations[i * 4 + 0]
+      )
+
+      let mtx = new THREE.Matrix4()
+      let max_value = this._updateCenterAndScaleData(
+        i,
+        mtx,
+        _center,
+        _scale,
+        _quaternion
+      )
+
+      let destOffset = this._loadedVertexCount * 8 + i * 4 * 2
+      for (let j = 0; j < cov_indexes.length; j++) {
+        rotationAndColorData_int16[destOffset + j] = parseInt(
+          (mtx.elements[cov_indexes[j]] * 32767.0) / max_value
+        )
+      }
+
+      // RGBA
+      destOffset = this._loadedVertexCount * 16 + (i * 4 + 3) * 4
+      rotationAndColorData_uint8[destOffset + 0] = colors[i * 4 + 0]
+      rotationAndColorData_uint8[destOffset + 1] = colors[i * 4 + 1]
+      rotationAndColorData_uint8[destOffset + 2] = colors[i * 4 + 2]
+      rotationAndColorData_uint8[destOffset + 3] = colors[i * 4 + 3]
+
+      // Store scale and transparent to remove splat in sorting process
+      new_positions[i * 4 + 0] = mtx.elements[12]
+      new_positions[i * 4 + 1] = mtx.elements[13]
+      new_positions[i * 4 + 2] = mtx.elements[14]
+      new_positions[i * 4 + 3] =
+        (Math.max(_scale.x, _scale.y, _scale.z) * colors[i * 4 + 3]) / 255.0
+    }
 
     let temp = new Float32Array(this._positions.length + new_positions.length)
     temp.set(this._positions)
@@ -432,7 +487,8 @@ class SplatMesh extends THREE.Mesh {
    */
   setDataFromBuffer(buffer, vertexCount) {
     this._loadedVertexCount = 0
-    this._pushDataToTexture(buffer, vertexCount)
+    this._updateDataFromBuffer(buffer, vertexCount)
+    this._updateTexture(vertexCount)
     return this
   }
 
@@ -443,25 +499,31 @@ class SplatMesh extends THREE.Mesh {
    * @returns {SplatMesh}
    */
   appendDataFromBuffer(buffer, vertexCount) {
-    this._pushDataToTexture(buffer, vertexCount)
+    this._updateDataFromBuffer(buffer, vertexCount)
+    this._updateTexture(vertexCount)
     return this
   }
 
   /**
    *
    * @param spzData
+   * @returns {SplatMesh}
    */
-  setDataFromAttribute(attribute) {
-    const bytes = this._getBytesFromAttribute(
-      attribute.numPoints,
-      attribute.positions,
-      attribute.scales,
-      attribute.colors,
-      attribute.alphas,
-      attribute.rotations
-    )
+  setDataFromSpz(spzData) {
     this._loadedVertexCount = 0
-    this._pushDataToTexture(bytes.buffer, attribute.numPoints)
+    return this
+  }
+
+  /**
+   *
+   * @param geometry
+   * @returns {SplatMesh}
+   */
+  setDataFromGeometry(geometry) {
+    this._loadedVertexCount = 0
+    const vertexCount = geometry.attributes.position.count
+    this._updateDataFromGeometry(geometry)
+    this._updateTexture(vertexCount)
     return this
   }
 }
