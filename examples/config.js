04d921f1721e5f4f95eb4f9f31b279720401858f
cavencj <cavencj@gmail.com>
2025-11-21 14:39:23 +0800

    update the cesium key

--- examples/config.js
+++ examples/config.js
@@ -2,7 +2,7 @@ const config = {
   // maptiler_key: 'get_your_own_OpIi9ZULNHzrESv6T2vL',
   maptiler_key: 'GhNJDabtJW2KhnZWhdq0',
   cesium_key:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2NTEwZTU2Yi0wOGEyLTQyZjgtOTJjNi04Mzc2NGRlNzA4NTkiLCJpZCI6MjU5LCJpYXQiOjE3NTY4NDExOTJ9._Y3MIsYgGKTVTpkEpKPNT0cQSa_hUocY0DdH7h0U-xM',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhN2VkNDM5ZS1jMDk0LTQ3NDItOTM5ZS00MzU3M2M1MTc2ZTkiLCJpZCI6MjU5LCJpYXQiOjE3NjIxODg4MDB9.ZZG574sONzeHxsX8HJMaL_ZiGA3dh_HrOxL7DrKRcd4',
 }
 
 export default config
