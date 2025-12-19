eb174566d84c04dfa0744fac106cf36ed5ac9300
Caven Chen <cavencj@gmail.com>
2025-12-19 21:35:49 +0800

    change the splat worker

--- examples/3d-tiles-3dgs.js
+++ examples/3d-tiles-3dgs.js
@@ -2,7 +2,7 @@ import maplibregl from 'maplibre-gl'
 import * as THREE from 'three'
 import * as MTP from '@dvt3d/maplibre-three-plugin'
 import config from './config.js'
-import { Tileset } from './src/index.js'
+import { Tileset, WorkerTaskProcessor } from './src/index.js'
 
 const map = new maplibregl.Map({
   container: 'map',
@@ -21,8 +21,13 @@ const mapScene = new MTP.MapScene(map)
 
 mapScene.addLight(new THREE.AmbientLight())
 
+const splatWorker = new WorkerTaskProcessor(
+  new URL('./src/wasm/splat/wasm_splat.worker.min.js', import.meta.url).href
+)
+await splatWorker.init()
 let tileset = new Tileset('http://localhost:8080/3dties-ggy/tileset.json', {
   splatThreshold: -0.0000001,
+  splatWorker: splatWorker,
 })
 
 tileset.autoDisableRendererCulling = true
